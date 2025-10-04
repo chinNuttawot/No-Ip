0b4b18e2e0227dc465ebd3fe35772dbeef0da39d
chin-m1 <znuttawot.s@pttdigital.com>
2025-10-05 02:08:42 +0700

    fix file type

--- db.js
+++ db.js
@@ -1,15 +1,11 @@
 // src/db.ts
 import { neon } from "@neondatabase/serverless";
 
-export type Env = {
-    NEON_DATABASE_URL: string;
-};
-
-export function getDb(env: Env) {
-    if (!env?.NEON_DATABASE_URL) {
-        throw new Error(
-            "NEON_DATABASE_URL is missing. In dev, create .dev.vars. In prod, use `wrangler secret put`."
-        );
-    }
-    return neon(env.NEON_DATABASE_URL);
+export function getDb(env) {
+  if (!env?.NEON_DATABASE_URL) {
+    throw new Error(
+      "NEON_DATABASE_URL is missing. In dev, create .dev.vars. In prod, use `wrangler secret put`."
+    );
+  }
+  return neon(env.NEON_DATABASE_URL);
 }
